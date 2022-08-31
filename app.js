6b232a816d131f6d9163e6b0ae1b25663cdeee6f
Fernando Orozco <forozcomercado@gmail.com>
2022-08-31 17:32:43 -0500

    ADD morgan

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 
+const morgan = require("morgan");
 const express = require("express");
 const app = express();
 const appRoutes = require("./routes/app.routes");
 
 app.set("PORT", process.env.PORT);
 
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => res.redirect("/user/home"));
