6cb05d795e94bf3601dd201014e5ba760c83ce2a
Fernando Orozco <forozcomercado@gmail.com>
2022-08-31 17:31:30 -0500

    ADD now we can receive json objects from post requests

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 const appRoutes = require("./routes/app.routes");
 
 app.set("PORT", process.env.PORT);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => res.redirect("/user/home"));
 app.use("/user", appRoutes);
 
