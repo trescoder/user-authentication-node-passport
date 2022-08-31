5534768d9afa4cba099e6d67dc2053ce20879fa2
Fernando Orozco <forozcomercado@gmail.com>
2022-08-31 17:31:46 -0500

    ADD method to sign in

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -2,6 +2,11 @@ function home(req, res) {
   res.status(200).send("<h1>First Attempt</h1>");
 }
 
+function signIn(req, res) {
+  res.status(200).json({ body: req.body });
+}
+
 module.exports = {
   home,
+  signIn,
 };
