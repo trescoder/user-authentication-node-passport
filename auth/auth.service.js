172eac7dd837fddfb89c82bb75f1ceefbb9bc2b3
Fernando Orozco <forozcomercado@gmail.com>
2022-08-31 21:57:47 -0500

    UPDATE now the generated token only last 1h

--- auth/auth.service.js
+++ auth/auth.service.js
@@ -1,7 +1,9 @@
 const jwt = require("jsonwebtoken");
 
 async function login(user) {
-  const token = jwt.sign({ sub: user.username }, process.env.JWT_SECRET);
+  const token = jwt.sign({ sub: user.username }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXP,
+  });
   return token;
 }
 
