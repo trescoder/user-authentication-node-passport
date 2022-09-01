31558952a1d24bfd08ca3ede10354166077753c7
Fernando Orozco <forozcomercado@gmail.com>
2022-08-31 21:04:21 -0500

    ADD route to get user profile ADD route to logout

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -3,13 +3,14 @@ const router = require("express").Router();
 const appRoutes = require("../controllers/app.controller");
 const isLoggedIn = require("../middlewares/isLoggedIn");
 
-router.get("/home", isLoggedIn, appRoutes.home);
+router.get("/home", appRoutes.home);
+router.get("/:username/profile", isLoggedIn, appRoutes.getUserProfile);
 router.post("/sign-up", appRoutes.signUp);
-
 router.post(
   "/sign-in",
   passport.authenticate("local", { failureRedirect: "/home" }),
   appRoutes.signIn
 );
+router.post("/log-out", appRoutes.logOut);
 
 module.exports = router;
