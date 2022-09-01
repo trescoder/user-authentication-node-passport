9180a9c69da424bfaebaa0f4fa12133eeb50d938
Fernando Orozco <forozcomercado@gmail.com>
2022-08-31 19:41:05 -0500

    ADD method to sign-up

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -1,9 +1,16 @@
+const UserService = require("../services/user.service");
+
 function home(req, res) {
   res.status(200).send("<h1>First Attempt</h1>");
 }
 
-function signIn(req, res) {
-  res.status(200).json({ body: req.body });
+async function signIn(req, res) {
+  const { username, password } = req.body;
+  const { status, data } = await UserService.createUserAccount({
+    username,
+    password,
+  });
+  res.status(status).json({ data });
 }
 
 module.exports = {
