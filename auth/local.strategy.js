965d6beb91dcb3eeb046f0701fd739dfa8509596
Fernando Orozco <forozcomercado@gmail.com>
2022-08-31 20:44:03 -0500

    UPDATE passport local strategy done

--- auth/local.strategy.js
+++ auth/local.strategy.js
@@ -3,17 +3,25 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const { comparePassword } = require("../config/password.manager");
 
-passport.use(
-  new LocalStrategy(async function verify(username, password, cb) {
-    const user = await userService.getUser(username);
-    if (!user) {
-      return cb(null, false, { message: "Incorrect username or password" });
-    }
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
 
-    if (!comparePassword(user.password, password)) {
-      return cb(null, false, { message: "Incorrect username or password" });
-    }
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
 
-    return cb(null, user);
-  })
-);
+const LS = new LocalStrategy(async function verify(username, password, cb) {
+  const user = await userService.getUser(username);
+  if (!user) {
+    return cb(null, false, { message: "Incorrect username or password" });
+  }
+
+  if (!comparePassword(user.password, password)) {
+    return cb(null, false, { message: "Incorrect username or password" });
+  }
+
+  return cb(null, user);
+});
+
+module.exports = LS;
