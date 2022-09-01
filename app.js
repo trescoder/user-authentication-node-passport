c4ebee59964ed796b8ab19ff808008cab8c9d0bf
Fernando Orozco <forozcomercado@gmail.com>
2022-08-31 20:11:06 -0500

    ADD helmet setup ADD express-session setup ADD passport setup

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 
 const morgan = require("morgan");
+const passport = require("passport");
+const session = require("express-session");
+const helmet = require("helmet");
 const express = require("express");
 const app = express();
 const appRoutes = require("./routes/app.routes");
@@ -8,8 +11,18 @@ const appRoutes = require("./routes/app.routes");
 app.set("PORT", process.env.PORT);
 
 app.use(morgan("dev"));
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET, // use to sign the session ID
+    resave: false, // forces the session to be saved back to the session store
+    saveUninitialized: false, // forces a session that is "uninitialized" to be saved to the store
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.get("/", (req, res) => res.redirect("/user/home"));
 app.use("/user", appRoutes);
 
